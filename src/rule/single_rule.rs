03098bd098b6e68d0a101a64947df4a88933410d
Xianyao Chen <owenchenxy@gmail.com>
2023-01-25 20:26:30 +0800

    [xianyao] trigger rules support hmac-sha1/256/512 validation

--- src/rule/single_rule.rs
+++ src/rule/single_rule.rs
@@ -1,10 +1,12 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, io, vec, ptr::eq, str::{from_utf8, from_utf8_unchecked}};
 
+use itertools::Itertools;
 use serde::{Serialize, Deserialize};
-
+use crypto::{digest::Digest, sha1::Sha1, hmac::Hmac, mac::Mac, sha2::{Sha256, Sha512}};
 
 use crate::parser::*;
-use regex::Regex;
+use regex::{Regex, bytes};
+
 
 #[derive(Debug, PartialEq, Serialize, Deserialize, Clone)]
 pub struct SingleRule {
@@ -60,22 +62,185 @@ impl SingleRule {
     }
 
     fn match_hmac_sha1(&self, http_request: &HashMap<String, String>) -> bool {
-        todo!()
+        let payload = http_request.get("Body");
+        let secret = &self.value;
+        let signature = match get_header_from_http_request(self.name.as_str(), http_request){
+            None => {
+                let msg = format!("Header [{}] not found in request headers", self.name);
+                log::warn!("{}", msg);
+                return false
+            },
+            Some(s) => s,
+        };
+        check_payload_signature(payload, secret, signature.as_str())
+    }
+
+    fn match_hmac_sha256(&self, http_request: &HashMap<String, String>) -> bool {
+        let payload = http_request.get("Body");
+        let secret = &self.value;
+        let signature = match get_header_from_http_request(self.name.as_str(), http_request){
+            None => {
+                let msg = format!("Header [{}] not found in request headers", self.name);
+                log::warn!("{}", msg);
+                return false
+            },
+            Some(s) => s,
+        };
+        check_payload_signature256(payload, secret, signature.as_str())
     }
 
     fn match_hmac_sha512(&self, http_request: &HashMap<String, String>) -> bool {
-        todo!()
-    }
+        let payload = http_request.get("Body");
+        let secret = &self.value;
+        let signature = match get_header_from_http_request(self.name.as_str(), http_request){
+            None => {
+                let msg = format!("Header [{}] not found in request headers", self.name);
+                log::warn!("{}", msg);
+                return false
+            },
+            Some(s) => s,
+        };
+        check_payload_signature512(payload, secret, signature.as_str())    }
 
     fn match_ip_whitelist(&self, http_request: &HashMap<String, String>) -> bool {
         todo!()
     }
-
-    fn match_hmac_sha256(&self, http_request: &HashMap<String, String>) -> bool {
-        todo!()
-    }
 }
 
+fn check_payload_signature(payload: Option<&String>, secret: &str, signature: &str)-> bool{
+    if secret == "" {
+        let err_msg = format!("signature validation secret can not be empty");
+        log::error!("{}", err_msg);
+        return false;
+	}
+	// Extract the signatures.
+	let signatures = extract_signatures(signature, "sha1=");
+	// Validate the MAC.
+	validate_mac(payload, &mut Hmac::new(Sha1::new(), secret.as_bytes()), signatures)
+}
+
+fn check_payload_signature256(payload: Option<&String>, secret: &str, signature: &str)-> bool{
+    if secret == "" {
+        let err_msg = format!("signature validation secret can not be empty");
+        log::error!("{}", err_msg);
+        return false;
+	}
+	// Extract the signatures.
+	let signatures = extract_signatures(signature, "sha1=");
+	// Validate the MAC.
+	validate_mac(payload, &mut Hmac::new(Sha256::new(), secret.as_bytes()), signatures)
+}
+
+fn check_payload_signature512(payload: Option<&String>, secret: &str, signature: &str)-> bool{
+    if secret == "" {
+        let err_msg = format!("signature validation secret can not be empty");
+        log::error!("{}", err_msg);
+        return false;
+	}
+	// Extract the signatures.
+	let signatures = extract_signatures(signature, "sha1=");
+	// Validate the MAC.
+	validate_mac(payload, &mut Hmac::new(Sha512::new(), secret.as_bytes()), signatures)
+}
+
+fn extract_signatures(source: &str, prefix: &str) -> Vec<String> {
+    if source.contains(","){
+        return extract_comma_separated_values(source, prefix);
+    }
+    vec![source.trim_start_matches(prefix).to_string()]
+}
+
+fn extract_comma_separated_values(source: &str, prefix: &str) -> Vec<String>{
+    let values: Vec<String> = source.split(",")
+        .map(|s|s.trim())
+        .filter(|s|s.starts_with(prefix))
+        .map(|s|s.trim_start_matches(prefix).to_string())
+        .collect();
+    values
+}
+
+fn validate_mac<T>(payload: Option<&String>, hasher: &mut T, signatures: Vec<String>) -> bool
+where T: Mac{
+    if let None = payload{
+        let msg = format!("HMAC validation failed due to empty payload !");
+        log::warn!("{}", msg);
+        return false;
+    }
+    hasher.input(payload.unwrap().as_bytes());
+    let actual_mac = hasher.result();
+    let expected_signature = actual_mac
+                                    .code()
+                                    .iter()
+                                    .format_with("", |byte, f| f(&format_args!("{:02x}", byte)))
+                                    .to_string();
+
+    signatures.iter()
+              .map(|s| s.eq(&expected_signature))
+              .collect::<Vec<bool>>()
+              .iter()
+              .any(|x| *x)
+}
+
+#[test]
+fn test_match_hmac_sha1(){
+    let mut request: HashMap<String, String> = HashMap::from([
+        ("Url".to_string(), "/webhook-test-1".to_string()),
+        ("User-Agent".to_string(), "curl/7.77.0".to_string()),
+        ("Version".to_string(), "HTTP/1.1".to_string()),
+        ("Method".to_string(), "POST".to_string()),
+        ("Accept".to_string(), "*/*".to_string()),
+        ("Host".to_string(), "127.0.0.1:7878".to_string()),
+        ("Content-Type".to_string(), "application/json".to_string()),
+        ("Content-Length".to_string(), "45".to_string()),
+        ("Body".to_string(), "{\"data\":{\"data2\":[\"val1\", \"val2\"], \"data3\": \"val3\"},\"data_s\":\"s_d\"}".to_string()),
+        ("Peer-Address".to_string(), "127.0.0.1:56020".to_string()),
+        ("X-Signature".to_string(), "".to_string()),
+    ]);
+
+    let single_rule = SingleRule{
+        kind: "hmac-sha1".to_string(),
+        value: "1234".to_string(),
+        source: "header".to_string(),
+        name: "X-Signature".to_string(),
+    };
+
+    let mut hasher = Hmac::new(Sha1::new(), single_rule.value.as_bytes());
+    hasher.input(request.get("Body").unwrap().as_bytes());
+    let actual_mac = hasher.result();
+    let actual_mac = actual_mac
+        .code()
+        .iter()
+        .format_with("", |byte, f| f(&format_args!("{:02x}", byte)))
+        .to_string();
+    let sig = format!("sha1=dcwwcwee, sha1={}", actual_mac);
+    request.insert("X-Signature".to_string(), sig.to_string());
+    assert!(single_rule.match_hmac_sha1(&request));
+}
+
+#[test]
+fn gen_mac(){
+    let payload = "";
+    let secret = "1";
+    let mut hasher = Hmac::new(Sha1::new(), secret.as_bytes());
+    hasher.input(payload.as_bytes());
+    println!("{:#?}", hasher.result().code());
+}
+
+#[test]
+fn test_extract_signature(){
+    let source = "sha1=8888";
+    let prefix = "sha1=";
+    assert_eq!(vec!["8888"], extract_signatures(source, prefix));
+    let source = "sha1=11111, sha1=22222, sha1=33333, sha256=44444, sha1=55555";
+    assert_eq!(vec!["11111","22222","33333","55555"], extract_signatures(source, prefix));
+}
+
+#[test]
+fn test_extract_comma_separated_values(){
+    let source = "sha1=11111, sha1=22222, sha1=33333, sha256=44444, sha1=55555";
+    let prefix = "sha1=";
+    assert_eq!(vec!["11111","22222","33333","55555"], extract_comma_separated_values(source, prefix))
+}
 
 #[test]
 fn test_match_value(){
